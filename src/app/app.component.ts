b84c7ba428b2662090b5a3c8f5d464afcd6982da
Wisnu Yumna <wisnu.yudhanta@gmail.com>
2026-02-01 03:52:21 +0700

    add analytics vercel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { inject as injectAnalytics } from '@vercel/analytics';
 import { MainLayoutComponent } from './core/layout/main-layout/main-layout.component';
 
 @Component({
@@ -7,6 +9,14 @@ import { MainLayoutComponent } from './core/layout/main-layout/main-layout.compo
   imports: [MainLayoutComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portf-angular';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      injectAnalytics();
+    }
+  }
 }
